b3ce3ab1008c8c72906c4c616e531d83d973953b
nabtron <developer@nabtron.com>
2016-05-04 20:34:13 +0500

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  this function (getFactorial) will return the value of the factorial for any entered number
+//  takes care of 1 and 0
+//
+//  Created by admin on 5/4/16.
+//  Copyright (c) 2016 nabtron. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int getFactorial(int num){
+    
+    int sum = 0;
+    if( (0 == num) || (1 == num) ){
+        sum = num;
+        return sum;
+    }
+    sum = getFactorial(num - 1) * num;
+    return sum;
+    
+}
+
+
+int main(int argc, const char * argv[])
+{
+
+//programming is so easy, really!
+
+    string num;
+    int intnum = 0;
+    cout << "Enter a number " << endl;
+    getline(cin, num);
+    intnum = stoi(num);
+    cout << getFactorial(intnum) << endl;
+    
+    return 0;
+}
